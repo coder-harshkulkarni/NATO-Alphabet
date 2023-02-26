79ad038f8fa85155a323d21bfa51162d37b14719
“Harsh <harshkulkarni78@gmail.com>
2023-02-26 12:57:52 +0530

    Caching Exception for KeyError

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import pandas
 data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_phonetic_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
-user_word = input("Enter the Word: ").upper()
-word_list = [nato_phonetic_alphabet[word] for word in user_word]
-print(word_list)
+no_error = True
+while no_error:
+    user_word = input("Enter the Word: ").upper()
+    try:
+        word_list = [nato_phonetic_alphabet[word] for word in user_word]
+    except KeyError:
+        print("Sorry, only letters in alphabets please.")
+    else:
+        no_error = False
+        print(word_list)
